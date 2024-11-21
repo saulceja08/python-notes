7e7373abca96205708bd474902b107b884211ddc
Saul Ceja <96592342+saulceja08@users.noreply.github.com>
2024-11-21 23:34:22 +0000

    Import packages and start basic code layout.

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
-cod_dataset = pd.read_csv('/workspaces/python-notes/cod.csv')
-print(cod_dataset.head(10))
+data_csv = pd.read_csv('/workspaces/python-notes/cod.csv')
+
+# Print the first 10 rows of the DataFrame
+print(data_csv.head(10))
+
+# Get and print the descriptive statistics for 'headshots'
+headshot_rank = data_csv['headshots'].describe()
+print("Headshot Rank Summary:\n", headshot_rank, "\n")
+
+# Get and print the descriptive statistics for 'wins'
+wins_rank = data_csv['wins'].describe()
+print("Wins Rank Summary:\n", wins_rank, "\n")
